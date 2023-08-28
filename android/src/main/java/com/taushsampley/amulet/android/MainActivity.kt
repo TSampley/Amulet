149c7d03ab75ca20f4ae1b9278417b7f8f144858
Taush Sampley <taushsampley@gmail.com>
2023-08-27 19:06:37 -0500

    Generate new project skeleton

---
+++ android/src/main/java/com/taushsampley/amulet/android/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.taushsampley.amulet.android
+
+import com.taushsampley.amulet.common.App
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                App()
+            }
+        }
+    }
+}
